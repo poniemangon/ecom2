f5f68ec62577010232cb890451885bbaacc3cab8
gon_14 <poniemangon@gmail.com>
2023-09-20 21:14:08 -0300

    estilos mas decentes

--- src/components/common/counter/Counter.jsx
+++ src/components/common/counter/Counter.jsx
@@ -1,23 +1,36 @@
 import React from "react";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
 const Counter = ({ counter, setCounter, onAdd }) => {
-   
   return (
-    <Box>
-      <Button onClick={()=>onAdd(counter)} variant="contained" sx={{ backgroundColor: "primary.main" }}>
-        Add to cart
-      </Button>
-      <Box>
+    <Box sx={{ display: "flex", flexDirection: "row" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          backgroundColor: "secondary.second",
+          marginRight: "3%",
+        }}
+      >
         <Button onClick={() => setCounter(counter + 1)}>+</Button>
-        <h5>{counter}</h5>
-        {counter > 1 && (
+        <Typography variant="subtitle1" sx={{ paddingTop: "5%" }}>
+          {counter}
+        </Typography>
+        {counter > 1 ? (
           <Button onClick={() => setCounter(counter - 1)}>-</Button>
+        ) : (
+          <Button>-</Button>
         )}
       </Box>
+      <Button
+        onClick={() => onAdd(counter)}
+        variant="contained"
+        sx={{ backgroundColor: "primary.main" }}
+      >
+        Add to cart
+      </Button>
     </Box>
   );
 };
 
 export default Counter;
-
